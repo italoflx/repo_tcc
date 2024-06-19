28b66253808bfac9c672b511b101a409cd30fdd3
emanuel italo <emanuelitalo81@gmail.com>
2024-06-19 02:20:59 -0300

    canal laranja finalizado

---
+++ testeCanaisLaranja.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+from utils import redimensionar_para_tela, bgr_to_cmyk
+
+imagem = cv2.imread(r'E:\projetos\imagensTcc\MotoG10_Finais\CamaraoLaranja8x1.jpg')
+cv2.imshow('original', redimensionar_para_tela(imagem))
+
+imagem_cmyk = bgr_to_cmyk(imagem)
+
+c, m, y, k = cv2.split(imagem_cmyk)
+
+imagem_cinza = cv2.cvtColor(imagem_cmyk, cv2.COLOR_BGR2GRAY)
+
+_, imagem_binaria = cv2.threshold(y, 80, 255, cv2.THRESH_BINARY)
+
+imagem_mascarada_final = cv2.bitwise_and(imagem, imagem, mask=imagem_binaria)
+
+cv2.imshow('canal amarelo', redimensionar_para_tela(y))
+cv2.imshow('imagem binaria', redimensionar_para_tela(imagem_binaria))
+cv2.imshow('imagem final', redimensionar_para_tela(imagem_mascarada_final))
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
